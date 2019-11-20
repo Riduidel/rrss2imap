eb4bb49df1ed748ebd76b318cee39588a7cec79e
Nicolas Delsaux <nicolas.delsaux@zenika.com>
2019-11-20 21:05:03 +0100

    More email address failure cases !

--- src/feed.rs
+++ src/feed.rs
@@ -371,6 +371,10 @@ fn sanitize_email(email:&String, domain:&String)->String {
         static ref EMAIL_AND_NAME_DETECTOR:Regex = 
             Regex::new("([[:alpha:]_%\\+\\-\\.]+@[[:alpha:]_%\\+\\-]+\\.[[:alpha:]_%\\+\\-]+{1,}) \\(([^\\)]*)\\)").unwrap();
     }
+    lazy_static! {
+        static ref BAD_CHARACTER_REMOVER:Regex = 
+            Regex::new("[^[:alnum:].]").unwrap();
+    }
     if EMAIL_AND_NAME_DETECTOR.is_match(email) {
         let captures = EMAIL_AND_NAME_DETECTOR.captures(email).unwrap();
         return format!("{} <{}>", captures.get(2).unwrap().as_str(), captures.get(1).unwrap().as_str());
@@ -382,11 +386,10 @@ fn sanitize_email(email:&String, domain:&String)->String {
             email
         };
         // Remove bad characters
-        let email = trim_to_chars(email, vec!["|", ":", "-", "<", ">"]);
-        let tuple = (email.clone(),
-        unidecode(&email).to_lowercase() // second element of tuple is generated user address
-                    .replace(" ", "_")
-                    .replace(",", "_")
+        let trimmed:String = trim_to_chars(email, vec!["|", ":", "-", "<", ">"]);
+        let lowercased = trimmed.to_lowercase();
+        let tuple = (trimmed.clone(),
+                    BAD_CHARACTER_REMOVER.replace_all(&lowercased, "_")
                 );
         return format!("{} <{}@{}>", tuple.0, tuple.1, domain);
     }
@@ -396,6 +399,16 @@ fn sanitize_email(email:&String, domain:&String)->String {
 mod tests {
     mod email_tests {
         use super::super::*;
+
+        #[test]
+        fn can_create_email_from_flo() {
+            assert_eq!("F(lo) <f_lo_@linuxfr.org>", sanitize_email(&"F(lo)".to_string(), &"linuxfr.org".to_string()));
+        }
+
+        #[test]
+        fn can_create_email_from_blog_a_part() {
+            assert_eq!("Blog à part <blog___part@alias.erdorin.org>", sanitize_email(&"Blog à part".to_string(), &"alias.erdorin.org".to_string()));
+        }
 
         #[test]
         fn can_create_email_from_xkcd() {
